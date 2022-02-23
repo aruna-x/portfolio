5dfba4e2ff6f40844c1cdf1441a0c6cac7390afd
Aruna <aruna.evan15@gmail.com>
2022-02-23 12:37:54 -0800

    update tech stack

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
       <h2>About</h2>
       <img src="/portfolio/images/headshot.jpg" alt="Aruna Evan" className="headshot"/>
       <p>Software Engineer invested in collaboration, dedication and empathy. Trained in designing, building, deploying, and maintaining full-stack web apps using React, Ruby on Rails, and Javascript-based programming. Passionate about exercising strong intellectual curiosity and attention to detail to the benefit of a company’s vision and to improve the human experience through technological innovation and solutions.</p>
-      <p><em><strong>Tech Stack:</strong> Ruby on Rails, React, Javascript, CSS, HTML, Redux, Heroku, PostgreSQL, Redis, Active Record, SQLite, SQL, Git, SourceTree</em></p>
+      <p><em><strong>Tech Stack:</strong> React, Ruby on Rails, Javascript, CSS, HTML, Redux, Apollo (GraphQL), Heroku, PostgreSQL, Redis, Active Record, SQLite, SQL, Git</em></p>
     </div>
   );
 }
